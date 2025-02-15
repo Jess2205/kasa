b3f6e856d05134c8684608021a2d5372ff055559
Isidore <jessica.muschke79@gmail.com>
2025-02-15 20:45:58 +0100

    Ajout d’un Header et d’un Footer qui entourent le AppRouter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import AppRouter from './routes/Router.jsx';
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRouter from "./routes/Router";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
-function App() {
-  return <AppRouter />;
-}
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <AppRouter />
+      <Footer />
+    </Router>
+  );
+};
 
 export default App;
