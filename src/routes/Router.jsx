23a8e4fc6acc7a067f6a0c9d7270d4720cca0e02
Isidore <jessica.muschke79@gmail.com>
2025-02-16 00:37:50 +0100

    Correction chemin d'accès

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,18 +1,16 @@
-// src/routes/Router.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import Logement from "../pages/Logement";
-import Page404 from "../pages/Page404";
+import { Routes, Route } from "react-router-dom";
+import Home from "/src/pages/Home/Home";
+import Logement from "/src/pages/Logement/Logement";
+import { Page404 } from "/src/pages/Page404/Page404";
+
 
 const AppRouter = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/logement/:id" element={<Logement />} />
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/logement/:id" element={<Logement />} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
   );
 };
 
