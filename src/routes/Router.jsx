0346af5bdd10bcc563e87af70aa495a4163bf705
Isidore <jessica.muschke79@gmail.com>
2025-02-16 16:00:34 +0100

    Import APropos dans Router

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "/src/pages/Home/Home";
 import Logement from "/src/pages/Logement/Logement";
+import APropos from "../pages/About/A-propos";  // Remonter d'un niveau pour aller dans /pages/About
+
 import { Page404 } from "/src/pages/Page404/Page404";
 
 
@@ -8,6 +10,7 @@ const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/A-propos" element={<APropos />} />
       <Route path="/logement/:id" element={<Logement />} />
       <Route path="*" element={<Page404 />} />
     </Routes>
