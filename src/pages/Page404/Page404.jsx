2406c73a98a997c4fe773ec160f5a1039a3f0615
Isidore <jessica.muschke79@gmail.com>
2025-03-19 15:50:34 +0100

    Correction page 404 h2 au lieu de span

--- src/pages/Page404/Page404.jsx
+++ src/pages/Page404/Page404.jsx
@@ -5,7 +5,7 @@ export function Page404() {
   return (
     <div className={styles.notFoundContainer}>
       <h1>404</h1>
-      <span>Oups! La page que vous demandez n&apos;existe pas.</span>
+      <h2>Oups! La page que vous demandez n&apos;existe pas.</h2>
       <Link to="/">Retour sur la page d&apos;accueil</Link>
     </div>
   );
